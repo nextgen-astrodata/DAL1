6a4525d51b6fb4a4fb8217acebd37a6891ae4d0f
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-04-20 12:46:26 +0000

    Some additional testing for the dalLopesEvent class; also added further data output for checking and plotting.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@292 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- test/tdalLopesEvent.cpp
+++ test/tdalLopesEvent.cpp
@@ -44,6 +44,7 @@
   \verbatim
   ./tdalLopesEvent 2007.01.31.23\:59\:33.960.event
   \endverbatim
+
 */
 
 // ------------------------------------------------------------------------------
@@ -83,6 +84,59 @@ void export_data (std::string const &filename,
   for (sample=0; sample<nofSamples; sample++) {
     for (antenna=0; antenna<nofAntennas; antenna++) {
       outfile << data(sample,antenna) << "\t";
+    }
+    outfile << std::endl;
+  }
+  outfile.close();
+  std::cout << "done" << std::endl;
+
+}
+
+// ------------------------------------------------------------------------------
+
+/*!
+  \brief Export the antenna-/channeldata to an ASCII-table
+
+  The data will be exported to a simple ASCII table which then in turn can be
+  plotted e.g. <a href="http://www.gnuplot.info">Gnuplot</a>:
+  \verbatim
+  set grid
+  set xlabel 'Sample number'
+  set ylabel 'ADC counts'
+  plot 'lopesevent_cpp.data' u 1 t 'Antenna 1' w l, 'lopesevent_cpp.data' u 2 t 'Antenna 2' w l
+  \endverbatim
+  In order to compare this to the output from the Blitz++ array, also run the
+  following:
+  \code
+  plot 'lopesevent.data' u 1 t 'Antenna 1 (Blitz++)' w l, 'lopesevent_cpp.data' u 1 t 'Antenna 1 (C++)' w l
+  \endcode
+  
+  \param filename    -- Name of the output file to which the data will be written
+  \param data        -- Array containing the actual data stored in original
+                        LOPES-Event file
+  \param nofAntennas -- Number of antennas in the data set
+  \param blocksize   -- Number of samples per channel/antenna
+*/
+void export_data (std::string const &filename,
+		  short *data,
+		  unsigned int const &nofAntennas,
+		  unsigned int const &blocksize)
+{
+  std::ofstream outfile;
+  unsigned int antenna (0);
+  unsigned int sample (0);
+  unsigned int datapoint (0);
+  
+  std::cout << "-- Name of output file      = " << filename    << std::endl;
+  std::cout << "-- nof. antennas            = " << nofAntennas << std::endl;
+  std::cout << "-- nof. samples per antenna = " << blocksize   << std::endl;
+
+  std::cout << "-- writing data to file ... " << std::flush;
+  outfile.open (filename.c_str());
+  for (sample=0; sample<blocksize; sample++) {
+    for (antenna=0; antenna<nofAntennas; antenna++) {
+      outfile << data[datapoint] << "\t";
+      datapoint++;
     }
     outfile << std::endl;
   }
@@ -165,7 +219,8 @@ int test_channeldata (std::string const &filename)
   std::cout << "\n[test_channeldata]\n" << std::endl;
 
   int nofFailedTests (0);
-  bool status (true);
+  unsigned int nofAntennas (0);
+  unsigned int blocksize (0);
 
   std::cout << "[1] Get all data as Blitz array..." << std::endl;
   try {
@@ -184,7 +239,7 @@ int test_channeldata (std::string const &filename)
   try {
     std::cout << "-- Opening file " << filename << " ..." << std::endl;
     dalLopesEvent event (filename);
-    unsigned int nofAntennas (event.nofAntennas());
+    nofAntennas = event.nofAntennas();
     blitz::Array<short,1> data (nofAntennas);
     std::cout << "-- Retrieving data for individual antennas ..." << std::endl;
     for (unsigned int ant(0); ant<nofAntennas; ant++) {
@@ -201,17 +256,17 @@ int test_channeldata (std::string const &filename)
 
     std::cout << "-- Opening file " << filename << " ..." << std::endl;
     dalLopesEvent event (filename);
+    nofAntennas = event.nofAntennas();
+    blocksize   = event.blocksize();
     std::cout << "-- Adjusting array to receive data ..." << std::endl;
     data = new short[event.nofDatapoints()];
     std::cout << "-- Retrieving data ..." << std::endl;
     data = event.data();
-    if (status) {
-      std::cout << "\t"
-		<< data[0] << " (" << &data[0] << ") .. "
-		<< data[1] << " (" << &data[1] << ") .. "
-		<< data[2] << " (" << &data[2] << ") "
-		<< std::endl;
-    }
+    // export data to file
+    export_data ("lopesevent_cpp.data",
+		 data,
+		 nofAntennas,
+		 blocksize);
   } catch (std::string message) {
     std::cerr << message << std::endl;
     nofFailedTests++;
@@ -220,8 +275,6 @@ int test_channeldata (std::string const &filename)
   std::cout << "[4] Get data per antenna as C++ array..." << std::endl;
   try {
     short *data;
-    unsigned int nofAntennas (0);
-    unsigned int blocksize (0);
 
     std::cout << "-- Opening file " << filename << " ..." << std::endl;
     dalLopesEvent event (filename);
@@ -286,8 +339,10 @@ int test_channeldata (std::string const &filename)
   set grid
   set xlabel 'Antenna number'
   set ylabel 'ADC counts'
-  plot 'lopesevent_statistics.data' u 1:2 t 'min(data)' w l, 'lopesevent_statistics.data' u 1:3 t 'mean(data)' w l, 'lopesevent_statistics.data' u 1:4 t 'max(data)' w l
-  plot 'lopesevent_statistics.data' u 1:5 t 'sum(data)' w
+  plot 'lopesevent_statistics.data' u 1:2 t 'min(data)' w l,\
+   'lopesevent_statistics.data' u 1:3 t 'mean(data)' w l,\
+   'lopesevent_statistics.data' u 1:4 t 'max(data)' w l
+  plot 'lopesevent_statistics.data' u 1:5 t 'sum(data)' w lp
   \endverbatim
  */
 int test_statistics (std::string const &filename)
@@ -326,7 +381,7 @@ int test_statistics (std::string const &filename)
 // ------------------------------------------------------------------------------
 
 /*!
-  \brief main routines of the test program
+  \brief Main routine of the test program
   
   \return nofFailedTests -- The number of failed tests
 */
