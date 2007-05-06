fd7bbcc0219f556c058d2c94ecef11fc49cf7041
masters <masters@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-05-06 21:17:13 +0000

    make script more interesting
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@320 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/tLopes.py
+++ implement/tLopes.py
@@ -2,24 +2,35 @@
 
 import dal
 from pylab import *
+font = {'fontname'	: 'Courier',
+	'color'		: 'r',
+	'fontweight'	: 'bold',
+	'fontsize'	: 11}
+title('A title', font, fontsize=12)
+xlabel('Sample number', font)
+ylabel('Amplitude', font)
 
 ds = dal.dalDataset()
 
 ret = ds.open("/home/jmasters/data/h5/lopes.h5")
 
-#stationGroup = ds.openGroup("Station")
+# stationGroup = ds.openGroup("Station")
 
-antennaTable = ds.openTable( "ANTENNA0", "Station" );
-#antennaTable.listColumns()
+ant0 = ds.openTable( "ANTENNA0", "Station" );
+ant1 = ds.openTable( "ANTENNA1", "Station" );
+ant2 = ds.openTable( "ANTENNA2", "Station" );
+ant3 = ds.openTable( "ANTENNA3", "Station" );
 
+# antennaTable.listColumns()
+
+tabs = ( ant0, ant1, ant2, ant3 )
+table = ant0
 outdata = dal.shortp()
 vals = []
-
-for x in range(antennaTable.getNumberOfRows()):
-	antennaTable.readRows(outdata,x,1)
+for x in range( table.getNumberOfRows() ):
+	table.readRows( outdata, x, 1 )
 	vals.append( outdata.value() )
-
-plot(vals[40000:50000])
+plot(vals)
 show()
-
 ds.close()
+print "SUCCESS"
