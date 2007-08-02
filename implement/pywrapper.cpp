5375d36ee0a3ea53bf0fa857ee950b5c7e96c9f6
masters <masters@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-08-02 16:01:13 +0000

    Add more class members.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@501 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/pywrapper.cpp
+++ implement/pywrapper.cpp
@@ -17,9 +17,17 @@ using namespace boost::python;
 BOOST_PYTHON_MODULE(dal)
 {
     class_<dalGroup>("dalGroup")
-         .def("getName", &dalGroup::getName)
-         .def("setName", &dalGroup::setName)
-         .def("getId", &dalGroup::getId)
+	.def(init<>())
+// 	.def(init<char*, void*>())
+ 	.def(init<hid_t,char*>())
+        .def("getName", &dalGroup::getName)
+        .def("setName", &dalGroup::setName)
+        .def("getId", &dalGroup::getId)
+        .def("getAttributes", &dalGroup::getAttributes)
+        .def("setAttribute_string", &dalGroup::setAttribute_string)
+        .def("setAttribute_int", &dalGroup::setAttribute_int)
+        .def("setAttribute_uint", &dalGroup::setAttribute_uint)
+        .def("setAttribute_double", &dalGroup::setAttribute_double)
     ;
     class_<dalDataset>("dalDataset")
 	.def(init<char*>())
@@ -27,6 +35,8 @@ BOOST_PYTHON_MODULE(dal)
         .def("open", &dalDataset::open)
         .def("close", &dalDataset::close)
         .def("getType", &dalDataset::getType)
+/*	.def("openTable", &dalDataset::openTable,
+		return_value_policy<manage_new_object>())*/
 	.def("createTable", &dalDataset::createTable,
 		return_value_policy<manage_new_object>())
 	.def("createTableInGroup", &dalDataset::createTable,
