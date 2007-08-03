297948ae368337db575c4df291d3aed17ca2757b
masters <masters@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-08-03 15:50:03 +0000

    Improve the boost python binding directives.  Now requires python2.5 and boost 1.34.0 for void pointer support.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@509 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/pywrapper.cpp
+++ implement/pywrapper.cpp
@@ -1,5 +1,5 @@
 #include <boost/python.hpp>
-using namespace boost::python;
+using namespace boost;
 
 #include "dal.h"
 #include "dalArray.h"
@@ -16,10 +16,25 @@ using namespace boost::python;
 
 BOOST_PYTHON_MODULE(dal)
 {
-    class_<dalGroup>("dalGroup")
-	.def(init<>())
-// 	.def(init<char*, void*>())
- 	.def(init<hid_t,char*>())
+    python::class_<dalDataset>("dalDataset")
+	.def(python::init<char*>())
+	.def(python::init<char*, string>())
+        .def("open", &dalDataset::open)
+        .def("close", &dalDataset::close)
+        .def("getType", &dalDataset::getType)
+/*	.def("openTable", &dalDataset::openTable,
+		return_value_policy<manage_new_object>())*/
+	.def("createTable", &dalDataset::createTable,
+		python::return_value_policy<python::manage_new_object>())
+	.def("createTableInGroup", &dalDataset::createTableInGroup,
+		python::return_value_policy<python::manage_new_object>())
+	.def("createGroup", &dalDataset::createGroup,
+		python::return_value_policy<python::manage_new_object>())
+    ;
+    python::class_<dalGroup>("dalGroup")
+	.def(python::init<>())
+	.def(python::init<char*, void*>())
+ 	.def(python::init<hid_t,char*>())
         .def("getName", &dalGroup::getName)
         .def("setName", &dalGroup::setName)
         .def("getId", &dalGroup::getId)
@@ -29,21 +44,36 @@ BOOST_PYTHON_MODULE(dal)
         .def("setAttribute_uint", &dalGroup::setAttribute_uint)
         .def("setAttribute_double", &dalGroup::setAttribute_double)
     ;
-    class_<dalDataset>("dalDataset")
-	.def(init<char*>())
-	.def(init<char*, string>())
-        .def("open", &dalDataset::open)
-        .def("close", &dalDataset::close)
-        .def("getType", &dalDataset::getType)
-/*	.def("openTable", &dalDataset::openTable,
-		return_value_policy<manage_new_object>())*/
-	.def("createTable", &dalDataset::createTable,
-		return_value_policy<manage_new_object>())
-	.def("createTableInGroup", &dalDataset::createTable,
-		return_value_policy<manage_new_object>())
+    python::class_<dalTable>("dalTable")
+        .def("getAttributes", &dalTable::getAttributes)
+	.def("openTable", &dalTable::openTable)
+	.def("createTable", &dalTable::createTable)
+	.def("addColumn", &dalTable::addColumn)
+	.def("addArrayColumn", &dalTable::addArrayColumn)
+	.def("addComplexColumn", &dalTable::addComplexColumn)
+	.def("removeColumn", &dalTable::removeColumn)
+	.def("writeDataByColNum", &dalTable::writeDataByColNum)
+	.def("appendRow", &dalTable::appendRow,
+		python::return_value_policy<python::return_opaque_pointer>())
+	.def("appendRows", &dalTable::appendRows)
+	.def("listColumns", &dalTable::listColumns)
+	.def("readRows", &dalTable::readRows)
+/*	.def("getAttribute", &dalTable::getAttribute,
+		python::return_value_policy<python::manage_new_object>())*/
+        .def("printColumns", &dalTable::printColumns)
+        .def("setAttribute_string", &dalTable::setAttribute_string)
+        .def("setAttribute_int", &dalTable::setAttribute_int)
+        .def("setAttribute_uint", &dalTable::setAttribute_uint)
+        .def("setAttribute_double", &dalTable::setAttribute_double)
+	.def("findAttribute", &dalTable::findAttribute)
     ;
-    class_<dalTable>("dalTable")
-         .def("getAttributes", &dalTable::getAttributes)
-         .def("printColumns", &dalTable::printColumns)
+    python::class_<dalColumn>("dalColumn")
+	.def(python::init<>())
+	.def(python::init<string>())
+	.def(python::init<string,string>())
+	.def("addMember", &dalColumn::addMember)
+	.def("getName", &dalColumn::getName)
+	.def("getType", &dalColumn::getType)
+	.def("getSize", &dalColumn::getSize)
     ;
 }
