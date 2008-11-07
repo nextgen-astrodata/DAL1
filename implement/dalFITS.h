5369bd9285e9c7a6e2abacc6e12102f19ccea35d
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-11-07 14:51:56 +0000

    Cleaning up of Doxygen comments and addition of GPL header
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@2129 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/dalFITS.h
+++ implement/dalFITS.h
@@ -1,16 +1,52 @@
-namespace DAL {
+/*-------------------------------------------------------------------------*
+ | $Id:: TBB_Timeseries.h 2126 2008-11-07 13:31:59Z baehren              $ |
+ *-------------------------------------------------------------------------*
+ ***************************************************************************
+ *   Copyright (C) 2008                                                    *
+ *   Sven Duscha ()                                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
+#ifndef DALFITS_H
+#define DALFITS_H
+
+namespace DAL {
+  
   /*!
-   \class dalFITS
-   \ingroup DAL
-   \brief DAL class to provide access to FITS
-   \author Sven Duscha
-   */
+    \class dalFITS
+
+    \ingroup DAL
+
+    \brief DAL class to provide access to FITS
+
+    \author Sven Duscha
+  */
   class dalFITS {
 
+  public:
+    
     //! Default constructor
     dalFITS ();
-
+    
+    //! Destructor
+    ~dalFITS ();
+    
   };
-
+  
 }
+
+#endif
