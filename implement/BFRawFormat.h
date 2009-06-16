7f4dd269de858fb10417153286317ba9c2dc7d9d
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-06-16 07:46:25 +0000

    [1] Improved Doxygen documentation of DAL components. [2] Continued implementation to create example images for the ongoing discussion on LOFAR image format.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@2673 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/BFRawFormat.h
+++ implement/BFRawFormat.h
@@ -20,12 +20,6 @@
 //#
 //#  $Id: BFRawFormat.h 12611 2009-03-11 14:04:07Z de jong $
 
-// Description:
-/*
-This class BFRawFormat defines the format of the BFRaw data type.
-This class is kept separate from the actual BFRaw class to seperate the data format from the functionality added to the BFRaw data type defined in the BFRaw class while making sure that there is only one location where the format is defined. The BFRaw class is derived from this class.
-*/
-		
 #ifndef BFRAWFORMAT_H
 #define BFRAWFORMAT_H
 
@@ -33,95 +27,119 @@ This class is kept separate from the actual BFRaw class to seperate the data for
 
 namespace DAL {
 
-class BFRawFormat {
-	
-	public:
+  /*!
+    \file BFRawFormat.h
+    
+    \ingroup DAL
+    
+    \brief Definition of the format of the BFRaw data type
+    
+    \author Alwin de Jong
 
-		static const short maxNrSubbands = 62;
-		
+    <h3>Prerequisite</h3>
+    
+    <ul type="square">
+      <li>DAL::BFRaw
+    </ul>
+    <h3>Synopsis</h3>
+    
+    This class BFRawFormat defines the format of the BFRaw data type.
+    
+    This class is kept separate from the actual BFRaw class to seperate the data
+    format from the functionality added to the BFRaw data type defined in the
+    BFRaw class while making sure that there is only one location where the
+    format is defined. The BFRaw class is derived from this class.
+  */
+  
+  class BFRawFormat {
+    
+  public:
+    
+    static const short maxNrSubbands = 62;
+    
     //! Components of the BFRaw header
     struct BFRaw_Header
-		{
+    {
       //! 0x3F8304EC, also determines endianness
-			uint32_t    magic;
+      uint32_t    magic;
       //! The number of bits per sample
-			uint8_t     bitsPerSample;
+      uint8_t     bitsPerSample;
       //! The number of polarizations
-			uint8_t     nrPolarizations;
+      uint8_t     nrPolarizations;
       //! Number of subbands
-			uint16_t    nrSubbands;
+      uint16_t    nrSubbands;
       //! 155648 (160Mhz) or 196608 (200Mhz)
-			uint32_t    nrSamplesPerSubband;
+      uint32_t    nrSamplesPerSubband;
       //! Name of the station
-			char      station[20];
+      char      station[20];
       //! The sample rate: 156250.0 or 195312.5 .. double
-			double   sampleRate;
+      double   sampleRate;
       //! The frequencies within a subband
-			double   subbandFrequencies[maxNrSubbands];
+      double   subbandFrequencies[maxNrSubbands];
       //! The beam pointing directions
-			double   beamDirections[8][2];
-			//! mapping from subbands to beams
-			int16_t     subbandToBeamMapping[maxNrSubbands];
+      double   beamDirections[8][2];
+      //! mapping from subbands to beams
+      int16_t     subbandToBeamMapping[maxNrSubbands];
       //! Padding to circumvent 8-byte alignment
-			uint32_t    padding;
-		} header;
-	
-		    //! Components of the header of a single block of raw data
-		struct BlockHeader
-		{
-      //! 0x2913D852
-			uint32_t      magic;
-			int32_t       coarseDelayApplied[8];
-      //! Padding to circumvent 8-byte alignment
-			uint8_t       padding[4];
-			double     fineDelayRemainingAtBegin[8];
-			double     fineDelayRemainingAfterEnd[8];
-      //! Compatible with TimeStamp class.
-			int64_t      time[8];
-			
-	//		uint32_t      nrFlagsRanges[8];
-			/*    
-			struct range
-			{
-				uint32_t    begin; // inclusive
-				uint32_t    end;   // exclusive
-			} flagsRanges[8][16];
-			*/
-			
-			struct marshalledFlags {
-				uint32_t      nrFlagsRanges;
-				struct range
-				{
-					uint32_t    begin; // inclusive
-					uint32_t    end;   // exclusive
-				} flagsRanges[16];
-			} flags[8];
-						
-		} block_header;
+      uint32_t    padding;
+    } header;
     
-		
+    //! Components of the header of a single block of raw data
+    struct BlockHeader
+    {
+      //! 0x2913D852
+      uint32_t      magic;
+      int32_t       coarseDelayApplied[8];
+      //! Padding to circumvent 8-byte alignment
+      uint8_t       padding[4];
+      double     fineDelayRemainingAtBegin[8];
+      double     fineDelayRemainingAfterEnd[8];
+      //! Compatible with TimeStamp class.
+      int64_t      time[8];
+      
+      //		uint32_t      nrFlagsRanges[8];
+      /*    
+	    struct range
+	    {
+	    uint32_t    begin; // inclusive
+	    uint32_t    end;   // exclusive
+	    } flagsRanges[8][16];
+      */
+      
+      struct marshalledFlags {
+	uint32_t      nrFlagsRanges;
+	struct range
+	{
+	  uint32_t    begin; // inclusive
+	  uint32_t    end;   // exclusive
+	} flagsRanges[16];
+      } flags[8];
+      
+    } block_header;
+    
+    
     // dataStruct is 8 bytes
-		
-		struct Sample
-		{
-			std::complex<int16_t> xx;
-			std::complex<int16_t> yy;
-		};
-		
-		
-	public:
-		    //! Get the number of bits per sample
-		inline uint8_t bitsPerSample () const { return this->header.bitsPerSample; }
+    
+    struct Sample
+    {
+      std::complex<int16_t> xx;
+      std::complex<int16_t> yy;
+    };
+    
+    
+  public:
+    //! Get the number of bits per sample
+    inline uint8_t bitsPerSample () const { return this->header.bitsPerSample; }
     //! Get the number of polarizations
-		inline uint8_t nofPolarizations () const { return this->header.nrPolarizations; }
+    inline uint8_t nofPolarizations () const { return this->header.nrPolarizations; }
     //! Get the number of beamlets
-		inline uint16_t nofSubbands () const { return this->header.nrSubbands;}
+    inline uint16_t nofSubbands () const { return this->header.nrSubbands;}
     //! Get the number of samples per beamlet
-		inline uint32_t nofSamplesPerSubband () const { return this->header.nrSamplesPerSubband;}
+    inline uint32_t nofSamplesPerSubband () const { return this->header.nrSamplesPerSubband;}
     //! Get the sample rate
-		inline double sampleRate () const { return this->header.sampleRate;}
-
-};
-
+    inline double sampleRate () const { return this->header.sampleRate;}
+    
+  };
+  
 }
 #endif  // BFRAWFORMAT_H
