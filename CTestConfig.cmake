2034c34f6716170a87ad8bbdf74a254d8886b515
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-09-03 10:34:56 +0000

    Moved Dashboard instructions out of the CMakeLists.txt file and into the already existing CTestConfig.cmake - this way settings can be changed more locally
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@2981 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,13 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "LOFAR User Software")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-SET (CTEST_DROP_METHOD xmlrpc)
-SET (CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
-SET (CTEST_DROP_SITE "usg.lofar.org:8081")
-SET (CTEST_DROP_LOCATION "DAL")
-#SET (CTEST_DROP_SITE_USER "lbaehren")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=LOFAR+User+Software")
+set(CTEST_DROP_SITE_CDASH TRUE)
