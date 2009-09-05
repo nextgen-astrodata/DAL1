e1264790df14247af5d78b7fa37a32340d8b746b
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-09-05 09:52:21 +0000

    [1] New test program for dealing with casacore images and their conversion to FITS. [2] Continued grouping source code.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@2990 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- implement/DataType.h
+++ implement/DataType.h
@@ -28,22 +28,21 @@
 #include <iostream>
 #include <string>
 
-namespace DAL   // Namespace DAL -- begin
-  {
-
+namespace DAL {  // Namespace DAL -- begin
+  
   /*!
     \class DataType
-
+    
     \ingroup DAL
-
+    
     \brief Type descriptors for standard LOFAR data products
-
+    
     \author Lars B&auml;hren
-
+    
     \date 2009/04/18
-
+    
     \test tDataType.cc
-
+    
     <h3>Prerequisite</h3>
 
     <ul type="square">
@@ -67,138 +66,133 @@ namespace DAL   // Namespace DAL -- begin
       <li>\b Type – descriptor for the file type
       <ul>
         <li>VIS – Visibility data
-  <li>SKY – (multi-dimensional) sky image
-  <li>RMC – Rotation measure cube
-  <li>RMM – Rotation measure map
-  <li>DYS – Dynamic spectrum
-  <li>CRC – Cosmic ray skymap cube
-  <li>BFS – Beamformed station data
-  <li>BFT – Beamformed tied-array data
-  <li>TBB – Transient buffer board dump (base-band and frequency data)
+	<li>SKY – (multi-dimensional) sky image
+	<li>RMC – Rotation measure cube
+	<li>RMM – Rotation measure map
+	<li>DYS – Dynamic spectrum
+	<li>CRC – Cosmic ray skymap cube
+	<li>BFS – Beamformed station data
+	<li>BFT – Beamformed tied-array data
+	<li>TBB – Transient buffer board dump (base-band and frequency data)
       </ul>
     </ul>
 
     <h3>Example(s)</h3>
 
   */
-  class DataType
+  class DataType {
+    
+  public:
+    
+    //! Type descriptors for standard LOFAR data products
+    enum Types
     {
-
-    public:
-
-      //! Type descriptors for standard LOFAR data products
-      enum Types
-      {
-        //! Beamformed station data
-        BFS,
-        //! Beamformed tied-array data
-        BFT,
-        //! Cosmic ray skymap (hyper-)cube, typically [Lon,Lat,Radius,Freq,Time]
-        CRC,
-        //! Dynamic spectrum, typically [Time,Freq]
-        DYS,
-        //! Rotation measure cube
-        RMC,
-        //! Rotation measure map
-        RMM,
-        //! (multi-dimensional) sky image, typically [Ra,Dec,Freq]
-        SKY,
-        //! Transient buffer board dump (base-band and frequency data)
-        TBB,
-        //! Visibility data
-        VIS
-      };
-
-    private:
-
-      //! Data type
-      DataType::Types type_p;
-
-    public:
-
-      // ------------------------------------------------------------- Construction
-
-      //! Default constructor
-      DataType (DataType::Types const &type=DataType::SKY);
-
-      /*!
-        \brief Copy constructor
-
-        \param other -- Another DataType object from which to create this new
-               one.
-      */
-      DataType (DataType const &other);
-
-      // -------------------------------------------------------------- Destruction
-
-      //! Destructor
-      ~DataType ();
-
-      // ---------------------------------------------------------------- Operators
-
-      /*!
-        \brief Overloading of the copy operator
-
-        \param other -- Another DataType object from which to make a copy.
-      */
-      DataType& operator= (DataType const &other);
-
-      // --------------------------------------------------------------- Parameters
-
-      //! Get the data type
-      inline DataType::Types type ()
-      {
-        return type_p;
-      }
-
-      //! Set the data type
-      inline void setType (DataType::Types const &type)
-      {
-        type_p = type;
-      }
-
-      //! Get the name of the data type
-      std::string name ();
-
-      /*!
-        \brief Get the name of the class
-
-        \return className -- The name of the class, DataType.
-      */
-      inline std::string className () const
-        {
-          return "DataType";
-        }
-
-      /*!
-        \brief Provide a summary of the internal status
-      */
-      inline void summary ()
-      {
-        summary (std::cout);
-      }
-
-      /*!
-        \brief Provide a summary of the internal status
-
-        \param os -- Output stream to which the summary is written.
-      */
-      void summary (std::ostream &os);
-
-      // ------------------------------------------------------------------ Methods
-
-      bool isVisibility ();
-
-    private:
-
-      //! Unconditional copying
-      void copy (DataType const &other);
-
-      //! Unconditional deletion
-      void destroy(void);
-
-    }; // Class DataType -- end
-
+      //! Beamformed station data
+      BFS,
+      //! Beamformed tied-array data
+      BFT,
+      //! Cosmic ray skymap (hyper-)cube, typically [Lon,Lat,Radius,Freq,Time]
+      CRC,
+      //! Dynamic spectrum, typically [Time,Freq]
+      DYS,
+      //! Rotation measure cube
+      RMC,
+      //! Rotation measure map
+      RMM,
+      //! (multi-dimensional) sky image, typically [Ra,Dec,Freq]
+      SKY,
+      //! Transient buffer board dump (base-band and frequency data)
+      TBB,
+      //! Visibility data
+      VIS
+    };
+    
+  private:
+    
+    //! Data type
+    DataType::Types type_p;
+    
+  public:
+    
+    // ------------------------------------------------------------- Construction
+    
+    //! Default constructor
+    DataType (DataType::Types const &type=DataType::SKY);
+    
+    /*!
+      \brief Copy constructor
+      
+      \param other -- Another DataType object from which to create this new
+             one.
+    */
+    DataType (DataType const &other);
+    
+    // -------------------------------------------------------------- Destruction
+    
+    //! Destructor
+    ~DataType ();
+    
+    // ---------------------------------------------------------------- Operators
+    
+    /*!
+      \brief Overloading of the copy operator
+      
+      \param other -- Another DataType object from which to make a copy.
+    */
+    DataType& operator= (DataType const &other);
+    
+    // --------------------------------------------------------------- Parameters
+    
+    //! Get the data type
+    inline DataType::Types type () {
+      return type_p;
+    }
+    
+    //! Set the data type
+    inline void setType (DataType::Types const &type) {
+      type_p = type;
+    }
+    
+    //! Get the name of the data type
+    std::string name ();
+    
+    /*!
+      \brief Get the name of the class
+      
+      \return className -- The name of the class, DataType.
+    */
+    inline std::string className () const {
+      return "DataType";
+    }
+    
+    /*!
+      \brief Provide a summary of the internal status
+    */
+    inline void summary () {
+      summary (std::cout);
+    }
+    
+    /*!
+      \brief Provide a summary of the internal status
+      
+      \param os -- Output stream to which the summary is written.
+    */
+    void summary (std::ostream &os);
+    
+    // ------------------------------------------------------------------ Methods
+    
+    bool isVisibility ();
+    
+  private:
+    
+    //! Unconditional copying
+    void copy (DataType const &other);
+    
+    //! Unconditional deletion
+    void destroy(void);
+    
+  }; // Class DataType -- end
+  
 } // Namespace DAL -- end
 
 #endif /* DATATYPE_H */
