a11327527c03bdd0a94ed68bb97c2a73c16c9cd5
fkbreitl <fkbreitl@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-09-25 08:48:43 +0000

    New features: - frequency and time units               - log intensity scale               - time range selection               - stop watch and progress information
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@3115 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- apps/spectrum.py
+++ apps/spectrum.py
@@ -6,27 +6,63 @@ import sys
 import pydal
 import scipy
 import pylab
+import time
 
 if (len(sys.argv) > 1):
   fn=sys.argv[1] #file.filename()
   file = pydal.BeamFormed(fn)
+  ds=pydal.dalDataset(fn,"HDF5")
+  #file.summary()
 else:
   print "Please provide a beam-formed hdf5 file as input."
   sys.exit(1)
 
-#file.summary()
+tmax=1  #time in seconds, 0 uses full range
+vmax=None #1e10 #limit intensity maximum, None is disabled
+interpolation='bilinear' #'bilinear'  'nearest'
+
+tstart=time.time()
 beam = file.getBeam(0)
-tmax=50000 #1376255
 nsub=beam.nofSubbands()
+beamg=ds.openGroup("beam000")
+fmin=beamg.getAttribute_int("CENTER_FREQUENCY_SB000")/1e6
+fmax=beamg.getAttribute_int("CENTER_FREQUENCY_SB"+str(nsub-1).zfill(3))/1e6
+ntot=ds.openTable("beam000/SB000").getNumberOfRows()
+tend=ntot*ds.getAttribute_int("DOWNSAMPLE_RATE") \
+         /ds.getAttribute_double("SAMPLE_RATE")
+if (tmax): ns=int(ntot*tmax/tend)
+else: ns=ntot
 
-spec= beam.getIntensity(0, 0, tmax)
-for i in range(1, nsub):
-  spec= scipy.vstack([spec, beam.getIntensity(i, 0, tmax)])
+#UTC=ds.getAttribute_string("EPOCH_UTC")
+#UTC_DATE=ds.getAttribute_string("EPOCH_DATE")
 
-pylab.imshow(spec, aspect='auto', interpolation='nearest', vmax=1e9)
+#print beam.ra()
+#sys.exit()
+
+for i in range(0, nsub):
+  try: spec
+  except NameError:    spec=beam.getIntensity(i, 0, ns)
+  print "reading subband", i, ", center frequency="
+  beamg.getAttribute_int("CENTER_FREQUENCY_SB"+str(i).zfill(3))
+  spec=scipy.vstack([spec, beam.getIntensity(i, 0, ns)])
+
+fs=20
+params = {'axes.labelsize': fs,
+          'axes.titlesize': 1.2*fs,
+          'text.fontsize': fs,
+          'legend.fontsize': fs,
+          'xtick.labelsize': .8*fs,
+          'ytick.labelsize': .8*fs}
+pylab.rcParams.update(params)
+
+pylab.imshow(pylab.log(spec), aspect='auto', extent=(0,tmax,fmax,fmin), interpolation=str(interpolation), vmax=vmax)
+#interpolation='nearest', aspect=.7*ns/nsub
 pylab.colorbar(fraction=0.07, pad=0)
-pylab.xlabel('time steps')
-pylab.ylabel('subband')
+pylab.xlabel('time [s]')
+pylab.ylabel('frequency [MHz]')
 pylab.title(fn)
-#pylab.savefig(fn+".png")
+#pylab.title("")
+pylab.savefig(fn+".png")
+print time.time()-tstart, "seconds"
 pylab.show()
+
