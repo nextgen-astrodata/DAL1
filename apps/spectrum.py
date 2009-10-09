8766a3a8813a81a2c360249acbe4ebe220cf1c92
fkbreitl <fkbreitl@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-10-09 20:39:47 +0000

    More improvements, suchs as tiling and saving of downsampled previews
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@3169 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- apps/spectrum.py
+++ apps/spectrum.py
@@ -4,19 +4,13 @@
 
 import pydal, pylab, numpy, time, sys, warnings
 
-if (len(sys.argv) < 2): sys.exit("Name to beam-formed.h5 file needed")
+#if (len(sys.argv) < 2): sys.exit("Usage: spectrum.py path")
 
-dt=100     #time in seconds, 0 uses full range
-imax=None #imax=None #1e10 #limit intensity maximum, None is disabled
-subbands=248; pixels=800; spectrum=numpy.empty((subbands,pixels))
-
-def config():
-  pylab.ion()
-  pylab.rcParams['font.size'] = 18
-  pylab.hold(False)
-  pylab.draw()
-  warnings.simplefilter("ignore",DeprecationWarning)
-config()
+dt=2     #time in seconds, 0 uses full range
+imax=14    #imax=None #1e10 #limit intensity maximum, None is disabled
+fmin=30; fmax=216; pixels=800;
+filenames=['obs108_CS302LBA.h5','obs108_RS307HBA.h5','obs108_RS503HBA.h5']
+#os.listdir('.') #sys.argv[1]
 
 def downsample(vector, n):
     """
@@ -28,46 +22,56 @@ def downsample(vector, n):
         return 0
     newvector = numpy.reshape(vector, (len(vector)/n, n))
     return numpy.add.reduce(newvector, 1)
+    
+pylab.ion()
+pylab.rcParams['font.size']=15
+pylab.hold(False)
+warnings.simplefilter("ignore",DeprecationWarning)
 
-filename=sys.argv[1]
-file = pydal.BeamFormed(filename)
-Beam = file.getBeam(0)
-DDS=pydal.dalDataset(filename,"HDF5")
-BG=DDS.openGroup("beam000")
-fmin=BG.getAttribute_int("CENTER_FREQUENCY_SB000")[0]/1e6
-fmax=240 #BG.getAttribute_int("CENTER_FREQUENCY_SB"+
-         #                str(Beam.nofSubbands()-1).zfill(3))/1e6
-samples=DDS.openTable("beam000/SB000").getNumberOfRows()
-duration=samples*DDS.getAttribute_int("DOWNSAMPLE_RATE") \
-         /DDS.getAttribute_double("SAMPLE_RATE")
-if(dt): ns=int(samples*dt/duration)
-else: ns=samples;
+Spectrum=numpy.empty((fmax-fmin,pixels))
+pylab.imshow(Spectrum,extent=(0,dt,fmax,fmin), \
+             aspect='auto', interpolation='nearest', vmax=imax)
+pylab.draw()
+offset=0
+for filename in filenames: 
+    DDS=pydal.dalDataset(filename,"HDF5")
+    samples=DDS.openTable("beam000/SB000").getNumberOfRows()
+    duration=samples*DDS.getAttribute_int("DOWNSAMPLE_RATE") \
+              /DDS.getAttribute_double("SAMPLE_RATE")
+    if(dt):
+        ns=int(samples*dt/duration)
+    else:
+        ns=samples
 
-ds=ns/pixels+1
-if ds*pixels>samples: ds=ds-1
-ns=ds*pixels
-dt=duration*ns/samples
-print "fmin=", fmin, fmax, dt, ds, ns, ns*pixels
+    ds=ns/pixels+1
+    if ds*pixels>samples: ds=ds-1
+    ns=ds*pixels
+    dt=duration*ns/samples
+    print "dt=",dt, ", ds=",ds, ", ns=",ns, ", ds*pixels=", ds*pixels
+    File = pydal.BeamFormed(filename)
+    Beam = File.getBeam(0)
+    for s in range(Beam.nofSubbands()):
+        tstart=time.time()
+        subband=downsample(Beam.getIntensity(s,0,ns), ds)
+        Spectrum[offset+s,]=numpy.sqrt(subband)
+        if(s % 5):
+            continue
+        else:
+            print 'duration:' , time.time()-tstart
+            sp=pylab.imshow(numpy.log(Spectrum),extent=(0,dt,fmax,fmin), \
+                            aspect='auto', interpolation='nearest', vmax=imax)
+            canvas=sp.figure.canvas
+            canvas.blit(sp.figure.bbox)
+    offset=offset+Beam.nofSubbands()
 
-for s in range(Beam.nofSubbands()):
-  tstart=time.time()
-  sb=downsample(Beam.getIntensity(s,0,ns), ds)
-  spectrum[s,]=numpy.sqrt(sb)
-  if(s % 5): continue
-  print 'duration:' , time.time()-tstart
-  sp=pylab.imshow(numpy.log(spectrum),extent=(0,dt,fmax,fmin), aspect='auto', \
-                  interpolation='nearest', vmax=imax)
-  canvas=sp.figure.canvas
-  canvas.blit(sp.figure.bbox)
-
-
-pylab.imshow(numpy.log(spectrum), extent=(0,dt,fmax,fmin), aspect='auto', \
-             vmax=imax, interpolation='nearest')
-#aspect=.7*ns/nsub
+pylab.imshow(numpy.log(Spectrum), extent=(0,dt,fmax,fmin), \
+             aspect='auto', vmax=imax, interpolation='nearest')
 pylab.colorbar(fraction=0.07, pad=0)
 pylab.xlabel('time [s]')
 pylab.ylabel('frequency [MHz]')
 pylab.title(filename)
+numpy.savez("preview.npz",Spectrum=Spectrum, dt=dt)
+#Spectrum=numpy.load("Spectrum.npz")
 #pylab.savefig(filename+".png")
 print time.time()-tstart, "seconds"
 pylab.show()
