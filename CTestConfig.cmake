b79980edb08e391274455923b086d2455c0d36b3
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-02-04 13:42:51 +0000

    Starting to use the new Dashboard set up for LOFAR software.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@4115 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,11 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-
-set(CTEST_PROJECT_NAME "LOFAR User Software")
+set(CTEST_PROJECT_NAME "DAL")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=LOFAR+User+Software")
+set(CTEST_DROP_SITE "svn.astron.nl")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=DAL")
 set(CTEST_DROP_SITE_CDASH TRUE)
