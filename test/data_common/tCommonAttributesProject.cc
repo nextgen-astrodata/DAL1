9edffdc6bf4e2c258b0cbed9782d160cab333145
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-12-06 10:21:02 +0000

    Basic tests for the CommonAttributesProject class.
    git-svn-id: http://usg.lofar.org/svn/code/trunk/src/DAL@6475 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- test/data_common/tCommonAttributesProject.cc
+++ test/data_common/tCommonAttributesProject.cc
@@ -24,6 +24,8 @@
 #include <CommonAttributesProject.h>
 
 // Namespace usage
+using std::cout;
+using std::endl;
 using DAL::CommonAttributesProject;
 
 /*!
@@ -50,20 +52,76 @@ using DAL::CommonAttributesProject;
 */
 int test_constructors ()
 {
-  std::cout << "\n[tCommonAttributesProject::test_constructors]\n" << std::endl;
+  cout << "\n[tCommonAttributesProject::test_constructors]\n" << endl;
 
   int nofFailedTests (0);
   
-  std::cout << "[1] Testing default constructor ..." << std::endl;
+  cout << "[1] Testing CommonAttributesProject() ..." << endl;
   try {
     CommonAttributesProject attr;
     //
     attr.summary(); 
   } catch (std::string message) {
-    std::cerr << message << std::endl;
+    std::cerr << message << endl;
     nofFailedTests++;
   }
   
+  cout << "[2] Testing CommonAttributesProject(string,string..) ..." << endl;
+  try {
+    std::string projectID ("LOFAR-001");
+    std::string projectTitle ("My little test Project");
+    std::string projectPI ("Dr. Frankenstein");
+    std::string projectCoI ("Dr. Jeckyl");
+    std::string projectContact ("frankenstein@lofar.org");
+
+    CommonAttributesProject attr (projectID,
+				  projectTitle,
+				  projectPI,
+				  projectCoI,
+				  projectContact);;
+
+    attr.summary(); 
+  } catch (std::string message) {
+    std::cerr << message << endl;
+    nofFailedTests++;
+  }
+  
+  return nofFailedTests;
+}
+
+//_______________________________________________________________________________
+//                                                              test_parameters
+
+/*!
+  \brief Test access to internal parameters
+
+  \return nofFailedTests -- The number of failed tests encountered within this
+          function.
+*/
+int test_parameters ()
+{
+  cout << "\n[tCommonAttributesProject::test_parameters]\n" << endl;
+
+  int nofFailedTests (0);
+  CommonAttributesProject attr;
+
+  attr.summary();
+
+  attr.setProjectID ("LOFAR-001");
+  attr.summary();
+
+  attr.setProjectTitle ("My little test project");
+  attr.summary();
+
+  attr.setProjectPI ("Dr. Frankenstein");
+  attr.summary();
+
+  attr.setProjectCoI ("Dr. Jeckyl");
+  attr.summary();
+
+  attr.setProjectContact ("frankenstein@lofar.org");
+  attr.summary();
+
   return nofFailedTests;
 }
 
@@ -76,6 +134,8 @@ int main ()
 
   // Test for the constructor(s)
   nofFailedTests += test_constructors ();
+  // Test access to internal parameters
+  nofFailedTests += test_parameters ();
 
   return nofFailedTests;
 }
