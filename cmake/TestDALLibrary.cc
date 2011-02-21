d584a5ecea6bb2b01f066fe6a5dfa01341705fe2
Lars Baehren <lbaehren@gmail.com>
2011-02-21 10:46:58 +0100

    Adding statements to return information with which configuration the DAL has been built.

--- cmake/TestDALLibrary.cc
+++ cmake/TestDALLibrary.cc
@@ -8,9 +8,25 @@ int main ()
     Library version
   */
   
+  std::cout << "DAL_VERSION "       << DAL_VERSION       << std::endl;
   std::cout << "DAL_VERSION_MAJOR " << DAL_VERSION_MAJOR << std::endl;
   std::cout << "DAL_VERSION_MINOR " << DAL_VERSION_MINOR << std::endl;
   std::cout << "DAL_VERSION_PATCH " << DAL_VERSION_PATCH << std::endl;
+
+  /*________________________________________________________
+    External packages
+  */
+  
+  std::cout << "DAL_WITH_BOOST "    << DAL_WITH_BOOST    << std::endl;
+  std::cout << "DAL_WITH_CFITSIO "  << DAL_WITH_CFITSIO  << std::endl;
+  std::cout << "DAL_WITH_CASACORE " << DAL_WITH_CASACORE << std::endl;
+  std::cout << "DAL_WITH_GSL "      << DAL_WITH_GSL      << std::endl;
+  std::cout << "DAL_WITH_HDF5 "     << DAL_WITH_HDF5     << std::endl;
+  std::cout << "DAL_WITH_LOFAR "    << DAL_WITH_LOFAR    << std::endl;
+  std::cout << "DAL_WITH_MYSQL "    << DAL_WITH_MYSQL    << std::endl;
+  std::cout << "DAL_WITH_NUMUTIL "  << DAL_WITH_NUMUTIL  << std::endl;
+  std::cout << "DAL_WITH_OPENMP "   << DAL_WITH_OPENMP   << std::endl;
+  std::cout << "DAL_WITH_WCSLIB "   << DAL_WITH_WCSLIB   << std::endl;
 
   return 1;
 }
