281b105e12b4ffcd437edca31a22c1d413c6ba68
Lars Baehren <lbaehren@gmail.com>
2011-02-24 16:13:03 +0100

    Implementation of FHS-compatible installation scheme.

--- cmake/InstallationSettings.cmake
+++ cmake/InstallationSettings.cmake
@@ -1,15 +1,41 @@
 
+## ==============================================================================
+#
+#    .                           DAL_INSTALL_PREFIX       [/opt/dal]
+#    ├── bin                     DAL_INSTALL_BINDIR       [DAL_INSTALL_PREFIX/bin]
+#    ├── include                 DAL_INSTALL_INCLUDEDIR   [DAL_INSTALL_PREFIX/include]
+#    ├── lib                     DAL_INSTALL_LIBDIR       [DAL_INSTALL_PREFIX/lib]
+#    └── share                   DAL_INSTALL_DATAROOTDIR  [DAL_INSTALL_PREFIX/share]
+#        ├── doc                 DAL_INSTALL_DOCDIR       [DAL_INSTALL_DATAROOTDIR/doc]
+#        │   └── html            DAL_INSTALL_HTMLDIR      [DAL_INSTALL_DOCDIR/html]
+#        ├── xml
+#        └── macros
+#
+## ==============================================================================
+
+## Set installation prefix
+
 if (DAL_INSTALL_PREFIX)
   set (CMAKE_INSTALL_PREFIX ${DAL_INSTALL_PREFIX} CACHE PATH
     "CMake installation area."
     FORCE
     )
 else (DAL_INSTALL_PREFIX)
-  set (CMAKE_INSTALL_PREFIX "/opt/dal" CACHE PATH
+  set (DAL_INSTALL_PREFIX "/opt/dal")
+  set (CMAKE_INSTALL_PREFIX ${DAL_INSTALL_PREFIX} CACHE PATH
     "CMake installation area."
     FORCE
     )
 endif (DAL_INSTALL_PREFIX)
+
+set (DAL_INSTALL_BINDIR       "${DAL_INSTALL_PREFIX}/bin"      )
+set (DAL_INSTALL_INCLUDEDIR   "${DAL_INSTALL_PREFIX}/include"  )
+set (DAL_INSTALL_LIBDIR       "${DAL_INSTALL_PREFIX}/lib"      )
+set (DAL_INSTALL_DATAROOTDIR  "${DAL_INSTALL_PREFIX}/share"    )
+set (DAL_INSTALL_DOCDIR       "${DAL_INSTALL_DATAROOTDIR}/doc" )
+set (DAL_INSTALL_HTMLDIR      "${DAL_INSTALL_DOCDIR}/html"     )
+
+## ==============================================================================
 
 # skip the full RPATH for the build tree
 set (CMAKE_SKIP_BUILD_RPATH YES)
