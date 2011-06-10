1ba57242ea7bd0ad572c7c59515a2089ea8a9b63
Lars Baehren <lbaehren@gmail.com>
2011-06-10 12:13:31 +0200

    Consolidation of test methods - quite a number of instructions are covered through the test for the base class already.

--- src/dal/data_hl/test/tBF_StokesDataset.cc
+++ src/dal/data_hl/test/tBF_StokesDataset.cc
@@ -41,237 +41,8 @@ using DAL::BF_StokesDataset;
   The generated HDF5 file will have the following structure:
   \verbatim
   tBF_StokesDataset.h5
-  |-- Dataset.002
-  |-- Dataset.003
-  |-- Dataset.004
-  |-- Dataset.006
-  |-- Dataset.007
-  |-- StokesI.002
-  |-- StokesI.003
-  |-- StokesQ
-  |-- Stokes101
-  |-- Stokes102
-  |-- Stokes103
-  `-- Stokes104
   \endverbatim
 */
-
-//_______________________________________________________________________________
-//                                                               test_HDF5Dataset
-
-/*!
-  \brief A few additional tests for working with the DAL::HDF5Dataset class
-
-  As the core functionality of the DAL::BF_StokesDataset class -- to deal with
-  a HDF5 dataset object -- is inherited from the DAL::HDF5Dataset class, we need
-  to ensure the additional parameters, which are part of the Stokes dataet, are
-  translated properly into the corresponding parameters dealing with the
-  underlying HDF5 object.
-
-  \param fileID -- Object identifier for the HDF5 file to work with
-
-  \return nofFailedTests -- The number of failed tests encountered within this
-          function.
-*/
-int test_HDF5Dataset (hid_t const &fileID)
-{
-  cout << "\n[tBF_StokesDataset::test_HDF5Dataset]\n" << endl;
-  
-  int nofFailedTests = 0;
-  bool status        = true;
-  hsize_t sidelength = 100000;
-  unsigned int rank  = 2;
-  std::string name;
-
-  /*_______________________________________________________________________
-    Test for the default constructor to check if all internal parameters
-    are being initialized.
-  */
-
-  cout << "[0] Testing HDF5Dataset() ..." << endl;
-  try {
-    DAL::HDF5Dataset dataset;
-    dataset.summary();
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  /*_______________________________________________________________________
-    Simplest constructor for HDF5Dataset will only open existing dataset,
-    as parameters for the creation of a new one are not available.
-  */
-
-  cout << "[1] Testing HDF5Dataset(hid_t, string) ..." << endl;
-  try {
-    name = "Dataset.001";
-    //
-    DAL::HDF5Dataset dataset (fileID,
-			      name);
-    dataset.summary();
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  /*_______________________________________________________________________
-    If unset, the chunking size will be initialized with the overall shape
-    of the dataset to be created; if however the resulting chunking size
-    exceeds limit for chunks to be buffered, the chunking dimensions are
-    adjusted automatically.
-  */
-
-  cout << "[2] Testing HDF5Dataset(hid_t, string, vector<hsize_t>) ..." << endl;
-  try {
-    name = "Dataset.002";
-    //
-    std::vector<hsize_t> shape (rank,sidelength);
-    //
-    DAL::HDF5Dataset dataset (fileID,
-			      name,
-			      shape);
-    dataset.summary();
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  /*_______________________________________________________________________
-    Create new dataset, explicitely specifying the chunking dimensions.
-  */
-
-  cout << "[3] Testing HDF5Dataset(hid_t, string, vector<hsize_t>, vector<hsize_t>) ..."
-       << endl;
-  try {
-    name = "Dataset.003";
-    //
-    std::vector<hsize_t> shape (rank,sidelength);
-    std::vector<hsize_t> chunk (rank,1000);
-    //
-    DAL::HDF5Dataset dataset (fileID,
-			      name,
-			      shape,
-			      chunk);
-    dataset.summary();
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-  
-  /*_______________________________________________________________________
-    Specify datatype of the individual array elements
-  */
-
-  cout << "[4] Testing HDF5Dataset(hid_t, string, vector<hsize_t>, hid_t) ..."
-       << endl;
-  try {
-    name = "Dataset.004";
-    //
-    std::vector<hsize_t> shape (rank,sidelength);
-    hid_t datatype = H5T_NATIVE_INT;
-    //
-    DAL::HDF5Dataset dataset (fileID,
-			      name,
-			      shape,
-			      datatype);
-    dataset.summary();
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  /*_______________________________________________________________________
-    Use the simple version of the HDF5Dataset::open method to a) access a
-    non-existing and b) an existing dataset within the file.
-  */
-
-  cout << "[5] Testing HDF5Dataset::open(hid_t, string)" << endl;
-  try {
-    DAL::HDF5Dataset dataset2;
-    DAL::HDF5Dataset dataset5;
-
-    // try to open non-existing dataset
-    name   = "Dataset.005";
-    status = dataset5.open (fileID, name);
-
-    if (status) {
-      cout << "--> [FAIL] Successfully opened dataset " << name << endl;
-      nofFailedTests++;
-    } else {
-      cout << "--> [OK] Faild to open dataset " << name << " - expected." << endl;
-    }
-    
-    // try to open existing dataset
-    name  = "Dataset.002";
-    status = dataset2.open (fileID, name);
-    
-    if (status) {
-      cout << "--> [OK] Successfully opened dataset " << name << endl;
-      dataset2.summary();
-    } else {
-      cout << "--> [FAIL] Faild to open dataset " << name << endl;
-      nofFailedTests++;
-    }
-    
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-  
-  /*_______________________________________________________________________
-    Use the simple version of the HDF5Dataset::open method to a) access a
-    non-existing and b) an existing dataset within the file.
-  */
-
-  cout << "[6] Testing HDF5Dataset::open(hid_t, string, vector<hsize_t>)" << endl;
-  try {
-    std::vector<hsize_t> shape (rank, sidelength);
-    DAL::HDF5Dataset dataset;
-
-    name   = "Dataset.006";
-    status = dataset.open (fileID, name, shape);
-
-    if (status) {
-      cout << "--> [OK] Successfully opened dataset " << name << endl;
-      dataset.summary();
-    } else {
-      cout << "--> [FAIL] Faild to open dataset " << name << endl;
-      nofFailedTests++;
-    }
-
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  /*_______________________________________________________________________
-    Explicitely specify chunking dimensions for dataset.
-  */
-
-  cout << "[7] Testing HDF5Dataset::open(hid_t, string, vector<hsize_t>, vector<hsize_t>)" << endl;
-  try {
-    std::vector<hsize_t> shape (rank, sidelength);
-    std::vector<hsize_t> chunk (rank, 1000);
-    DAL::HDF5Dataset dataset;
-
-    name   = "Dataset.007";
-    status = dataset.open (fileID, name, shape, chunk);
-
-    if (status) {
-      cout << "--> [OK] uccessfully opened dataset " << name << endl;
-      dataset.summary();
-    } else {
-      cout << "--> [FAIL] Failed to open dataset " << name << endl;
-      nofFailedTests++;
-    }
-
-  } catch (std::string message) {
-    std::cerr << message << endl;
-    nofFailedTests++;
-  }
-
-  return nofFailedTests;
-}
 
 //_______________________________________________________________________________
 //                                                              test_constructors
@@ -286,14 +57,15 @@ int test_HDF5Dataset (hid_t const &fileID)
 */
 int test_constructors (hid_t const &fileID)
 {
-  cout << "\n[tBF_StokesDataset::test_constructors]\n" << endl;
+  cout << "\n[tBF_StokesDataset::test_constructors]" << endl;
 
   int nofFailedTests       = 0;
-  unsigned int index       = 0;
-  unsigned int nofSamples  = 1000;
-  unsigned int nofSubbands = 36;
-  unsigned int nofChannels = 128;
-  std::vector<hsize_t> shape (2);
+  unsigned int rank        = 2;     /* Rank of the Stokes dataset           */
+  unsigned int index       = 0;     /* Index number of the Stokes dataset   */
+  unsigned int nofSamples  = 1000;  /* nof. samples along the time axis     */
+  unsigned int nofSubbands = 36;    /* nof. frequency sub-bands             */
+  unsigned int nofChannels = 128;   /* nof. frequency channels per sub-band */
+  std::vector<hsize_t> shape (rank);
   std::string nameDataset;
 
   shape[0] = nofSamples;
@@ -303,7 +75,7 @@ int test_constructors (hid_t const &fileID)
     Test 1: Default constructor (no dataset created)
   */
   
-  cout << "[1] Testing BF_StokesDataset() ..." << endl;
+  cout << "\n[1] Testing BF_StokesDataset() ..." << endl;
   try {
     BF_StokesDataset stokes;
     //
@@ -314,15 +86,12 @@ int test_constructors (hid_t const &fileID)
   }
   
   /*_______________________________________________________________________
-    Test 2: Argumented constructor to open existing dataset
-            Use constuctor on a non-existing dataset; if not enough
-	    parameters are defined to create a new dataset, the resulting
-	    object will not be connected to any valid dataset.
+    Test 2: Argumented constructor to open existing dataset.
   */
   
-  cout << "[2] Testing BF_StokesDataset(hid_t, string) ..." << endl;
+  cout << "\n[2] Testing BF_StokesDataset(hid_t, string) ..." << endl;
   try {
-    index       = 2;
+    index       = 0;
     nameDataset = BF_StokesDataset::getName(index);
     BF_StokesDataset stokes (fileID, nameDataset);
     stokes.summary(); 
@@ -332,14 +101,28 @@ int test_constructors (hid_t const &fileID)
   }
   
   /*_______________________________________________________________________
-    Test 3: Argumented constructor, containing minimal required parameters
+    Test 3: Argumented constructor to open existing dataset.
+  */
+  
+  cout << "\n[3] Testing BF_StokesDataset(hid_t, uint) ..." << endl;
+  try {
+    index       = 0;
+    BF_StokesDataset stokes (fileID, index);
+    stokes.summary(); 
+  } catch (std::string message) {
+    std::cerr << message << endl;
+    nofFailedTests++;
+  }
+  
+  /*_______________________________________________________________________
+    Test 4: Argumented constructor, containing minimal required parameters
             for creation of a new dataset.
   */
 
-  cout << "[3] Testing BF_StokesDataset(hid_t, string, vector<hsize_t>) ..."
+  cout << "\n[4] Testing BF_StokesDataset(hid_t, string, vector<hsize_t>) ..."
 	    << endl;
   try {
-    index = 3;
+    index = 4;
     BF_StokesDataset stokes (fileID, index, shape);
     //
     stokes.summary(); 
@@ -349,13 +132,13 @@ int test_constructors (hid_t const &fileID)
   }
 
   /*_______________________________________________________________________
-    Test 4: Argumented constructor.
+    Test 5: Argumented constructor.
   */
 
-  cout << "[4] Testing BF_StokesDataset(hid_t, string, vector<hsize_t>, Stokes::Component) ..."
+  cout << "\n[5] Testing BF_StokesDataset(hid_t, string, vector<hsize_t>, Stokes::Component) ..."
 	    << endl;
   try {
-    index = 4;
+    index = 5;
     BF_StokesDataset stokes (fileID,
 			     index,
 			     shape,
@@ -368,14 +151,14 @@ int test_constructors (hid_t const &fileID)
   }
   
   /*_______________________________________________________________________
-    Test 5: Argumented constructor providing individual values for the
+    Test 6: Argumented constructor providing individual values for the
             number of bins along the time axis, the number of frequency
 	    bands and the number of frequency channels per frequency band.
   */
 
-  cout << "[5] Testing BF_StokesDataset(hid_t,string,uint,uint,uint,Stokes::Component) ..." << endl;
+  cout << "\n[6] Testing BF_StokesDataset(hid_t,string,uint,uint,uint,Stokes::Component) ..." << endl;
   try {
-    index = 5;
+    index = 6;
     BF_StokesDataset stokes (fileID,
 			     index,
 			     nofSamples,
@@ -390,15 +173,15 @@ int test_constructors (hid_t const &fileID)
   }
 
   /*_______________________________________________________________________
-    Test 6: Argumented constructor providing individual values for the
+    Test 7: Argumented constructor providing individual values for the
             number of bins along the time axis, the number of frequency
 	    bands and the number of frequency channels per frequency band.
   */
 
-  cout << "[6] Testing BF_StokesDataset(hid_t,string,uint,vector<uint>,Stokes::Component) ..." << endl;
+  cout << "\n[7] Testing BF_StokesDataset(hid_t,string,uint,vector<uint>,Stokes::Component) ..." << endl;
   try {
     std::vector<unsigned int> channels (nofSubbands);
-    index       = 6;
+    index       = 7;
     nameDataset = BF_StokesDataset::getName(index);
 
     /* Assign number of channels per sub-band */
@@ -420,10 +203,10 @@ int test_constructors (hid_t const &fileID)
   }
 
   /*_______________________________________________________________________
-    Test 7: Copy constructor
+    Test 8: Copy constructor
   */
 
-  cout << "[7] Testing BF_StokesDataset(BF_StokesDataset) ..." << endl;
+  cout << "[8] Testing BF_StokesDataset(BF_StokesDataset) ..." << endl;
   try {
     index = 3;
     // Create the first of the two objects ...
@@ -761,9 +544,6 @@ int main ()
   /* If file creation was successful, run the tests. */
   if (H5Iis_valid(fileID)) {
 
-    // Additional tests for working with the DAL::HDF5Dataset class
-    nofFailedTests += test_HDF5Dataset (fileID);
-    
     // Test for the constructor(s)
     nofFailedTests += test_constructors (fileID);
     // // Test access to the attributes
