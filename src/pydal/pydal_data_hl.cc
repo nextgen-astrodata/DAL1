f03a899add3a28280f9395efeadf9f507c8c4616
duscha <duscha@astron.nl>
2011-06-17 06:14:24 +0800

    BF_RootGroup function declarations for Python bindings, and low-level Boost::BPL funcationality

--- src/pydal/pydal_data_hl.cc
+++ src/pydal/pydal_data_hl.cc
@@ -91,26 +91,63 @@ void export_BF_RootGroup ()
       //void ( BF_RootGroup::*summary)() = &BF_RootGroup::summary)
       .def( "CommonAttributes", &BF_RootGroup::commonAttributes, 
       "Get the common attributes of the class." )      
+      
+      .def( "nofSubArrayPointings", &BF_RootGroup::nofSubArrayPointings,
+      "Get the number of SubArray pointings." )
       ;
       
-      // Data access methods       
-      bool ( BF_RootGroup::*open)(hid_t const &,
-                string const &,
-                DAL::IO_Mode const &) = &BF_RootGroup::open;
+   // Data access methods       
+`   bool ( BF_RootGroup::*open)(hid_t const &,
+            string const &,
+            DAL::IO_Mode const &) = &BF_RootGroup::open;
 
-      bool ( BF_RootGroup::*openSubArrayPointing)( unsigned int const &,
-                DAL::IO_Mode const &) = &BF_RootGroup::openSubArrayPointing;
+   bool ( BF_RootGroup::*openSubArrayPointing)( unsigned int const &,
+            DAL::IO_Mode const &) = &BF_RootGroup::openSubArrayPointing;
 
-      bool ( BF_RootGroup::*openBeam)(hid_t const &,
-               const string &,
-               DAL::IO_Mode const &) = &BF_RootGroup::open;
+   bool ( BF_RootGroup::*openBeam1)(hid_t const &,
+            const string &,
+            DAL::IO_Mode const &) = &BF_RootGroup::open;
       
-      bool ( BF_RootGroup::*openStokesDataset)(unsigned const &,
-               unsigned int const &,
-               unsigned int const &,
-               DAL::IO_Mode const &) = &BF_RootGroup::openStokesDataset;
+   bool ( BF_RootGroup::*openStokesDataset1)(unsigned int const &,
+            unsigned int const &,
+            unsigned int const &,
+            DAL::IO_Mode const &) = &BF_RootGroup::openStokesDataset;
 
-      ;
+   bool ( BF_RootGroup::*openBeam2)(unsigned int const &,
+               unsigned const int &,
+               DAL::IO_Mode const &) = &BF_RootGroup::openBeam;
+      
+   bool ( BF_RootGroup::*openStokesDataset2)(unsigned int const &,
+            unsigned int const &,
+            unsigned int const &,
+            unsigned int const &,
+            unsigned int const &,
+            unsigned int const &,
+            DAL::Stokes::Component const &,
+            hid_t const &,
+            DAL::IO_Mode const &) = &BF_RootGroup::openStokesDataset;
+
+   bool ( BF_RootGroup::*openStokesDataset3)(unsigned int const &pointingID,
+            unsigned int const &beamID,
+            unsigned int const &stokesID,
+            unsigned int const &nofSamples,
+            unsigned int const &nofSubbands,
+            unsigned int const &nofChannels,
+            DAL::Stokes::Component const &component,
+            hid_t const &datatype=H5T_NATIVE_FLOAT,
+            DAL::IO_Mode const &flags);
+
+   bool ( BF_RootGroup::*openStokesDataset)(unsigned int const &pointingID,
+            unsigned int const &beamID,
+            unsigned int const &stokesID,
+            unsigned int const &nofSamples,
+            std::vector<unsigned int> const &nofChannels,
+            DAL::Stokes::Component const &component=DAL::Stokes::I,
+            hid_t const &datatype=H5T_NATIVE_FLOAT,
+            DAL::IO_Mode const &flags);
+
+   BF_BeamGroup ( BF_RootGroup::*getBeamGroup)(unsigned int const &pointingID,
+			       unsigned int const &beamID);
 }
 
 
@@ -467,3 +504,26 @@ void export_LOPES_EventFile ()
 	  "Get the number of data points stored." )
     ;
 }
+
+
+// ==============================================================================
+//
+//                                                                   BF_RootGroup
+//
+// ==============================================================================
+            
+ /*           
+            bpl::numeric::array BeamGroup::getIntensity_boost( int subband,
+						   int start,
+						   int length )
+{
+  float * values = NULL;
+  values = getIntensity( subband, start, length );
+  std::vector<int> mydims;
+  mydims.push_back( length );
+  bpl::numeric::array narray = num_util::makeNum( values, mydims );
+  delete [] values;
+  values = NULL;
+  return narray;
+}
+*/
