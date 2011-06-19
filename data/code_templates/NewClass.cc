daf27c0e25f307a5870cfc9677de35085c87df53
Lars Baehren <lbaehren@gmail.com>
2011-06-20 01:46:49 +0200

    Pull variable declarations out of function body, to allow them to be set from the command line.

--- data/code_templates/NewClass.cc
+++ data/code_templates/NewClass.cc
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "<newClass>.h"
+#include <<newModule>/<newClass>.h>
 
 namespace <namespace> { // Namespace <namespace> -- begin
   
