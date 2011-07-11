3c37160aea3e51a4bd695668756a5308b5bc0675
Lars Baehren <lbaehren@gmail.com>
2011-07-11 16:54:07 +0200

    Base object for DAL classes, definition common interface and some basic book-keeping of properties.

---
+++ src/dal/core/dalObjectBase.cc
@@ -0,0 +1,123 @@
+/***************************************************************************
+ *   Copyright (C) 2011                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <core/dalObjectBase.h>
+
+namespace DAL { // Namespace DAL -- begin
+  
+  // ============================================================================
+  //
+  //  Construction
+  //
+  // ============================================================================
+  
+  dalObjectBase::dalObjectBase ()
+  {;}
+  
+  /*!
+    \param other -- Another dalObjectBase object from which to create this new
+           one.
+  */
+  dalObjectBase::dalObjectBase (dalObjectBase const &other)
+  {
+    copy (other);
+  }
+  
+  // ============================================================================
+  //
+  //  Destruction
+  //
+  // ============================================================================
+  
+  dalObjectBase::~dalObjectBase ()
+  {
+    destroy();
+  }
+  
+  void dalObjectBase::destroy ()
+  {;}
+  
+  // ============================================================================
+  //
+  //  Operators
+  //
+  // ============================================================================
+  
+  //_____________________________________________________________________________
+  //                                                                    operator=
+  
+  /*!
+    \param other -- Another dalObjectBase object from which to make a copy.
+  */
+  dalObjectBase& dalObjectBase::operator= (dalObjectBase const &other)
+  {
+    if (this != &other) {
+      destroy ();
+      copy (other);
+    }
+    return *this;
+  }
+  
+  //_____________________________________________________________________________
+  //                                                                         copy
+  
+  /*!
+    \param other -- Another dalObjectBase object from which to make a copy.
+  */
+  void dalObjectBase::copy (dalObjectBase const &other)
+  {
+    itsFiletype = other.itsFiletype;
+    itsFlags    = other.itsFlags;
+  }
+
+  // ============================================================================
+  //
+  //  Parameters
+  //
+  // ============================================================================
+  
+  //_____________________________________________________________________________
+  //                                                                      summary
+  
+  /*!
+    \param os -- Output stream to which the summary is written.
+  */
+  void dalObjectBase::summary (std::ostream &os)
+  {
+    os << "[dalObjectBase] Summary of internal parameters." << std::endl;
+  }
+  
+  // ============================================================================
+  //
+  //  Public methods
+  //
+  // ============================================================================
+  
+  
+
+  // ============================================================================
+  //
+  //  Static methods
+  //
+  // ============================================================================
+  
+  
+
+} // Namespace DAL -- end
---
+++ src/dal/core/dalObjectBase.h
@@ -0,0 +1,139 @@
+/***************************************************************************
+ *   Copyright (C) 2011                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef DALOBJECTBASE_H
+#define DALOBJECTBASE_H
+
+// Standard library header files
+#include <iostream>
+#include <string>
+
+#include <core/dalFileType.h>
+#include <core/IO_Mode.h>
+
+namespace DAL { // Namespace DAL -- begin
+  
+  /*!
+    \class dalObjectBase
+    
+    \ingroup DAL
+    \ingroup core
+    
+    \brief Brief description for class DAL::dalObjectBase
+    
+    \author Lars B&auml;hren
+
+    \date 2011-07-11
+
+    \test tdalObjectBase.cc
+    
+    <h3>Prerequisite</h3>
+    
+    <ul type="square">
+      <li>[start filling in your text here]
+    </ul>
+    
+    <h3>Synopsis</h3>
+    
+    <h3>Example(s)</h3>
+    
+  */  
+  class dalObjectBase {
+
+  protected:
+
+    //! File type: CASA_MS, HDF5, FITS, etc.
+    dalFileType itsFiletype;
+    //! I/O mode flags
+    IO_Mode itsFlags;
+    
+  public:
+    
+    // === Construction =========================================================
+    
+    //! Default constructor
+    dalObjectBase ();
+    
+    //! Copy constructor
+    dalObjectBase (dalObjectBase const &other);
+    
+    // === Destruction ==========================================================
+
+    //! Destructor
+    ~dalObjectBase ();
+    
+    // === Operators ============================================================
+    
+    //! Overloading of the copy operator
+    dalObjectBase& operator= (dalObjectBase const &other); 
+    
+    // === Parameter access =====================================================
+
+    inline DAL::dalFileType::Type filetype () {
+      return itsFiletype.type();
+    }
+    
+    inline std::string filetypeName () {
+      return itsFiletype.name();
+    }
+    
+    //! Get object I/O mode flags
+    inline int flags () const {
+      return itsFlags.flags();
+    }
+
+    /*!
+      \brief Get the name of the class
+      \return className -- The name of the class, dalObjectBase.
+    */
+    inline std::string className () const {
+      return "dalObjectBase";
+    }
+
+    //! Provide a summary of the object's internal parameters and status
+    inline void summary () {
+      summary (std::cout);
+    }
+
+    //! Provide a summary of the object's internal parameters and status
+    void summary (std::ostream &os);    
+
+    // === Public methods =======================================================
+    
+    
+    
+    // === Static methods =======================================================
+    
+    
+    
+  private:
+    
+    //! Unconditional copying
+    void copy (dalObjectBase const &other);
+    
+    //! Unconditional deletion 
+    void destroy(void);
+    
+  }; // Class dalObjectBase -- end
+  
+} // Namespace DAL -- end
+
+#endif /* DALOBJECTBASE_H */
+  
